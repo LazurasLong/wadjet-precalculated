7ea48913e48cbae4cfe3226174bf43cd78c86999
Shuhei Nomura <info@danmaq.com>
2018-01-01 22:50:04 +0900

    Implemented.

--- .tools/clean.cmd
+++ .tools/clean.cmd
@@ -3,4 +3,7 @@
 CD /d %~dp0\..
 ECHO Clean up temporary files.
 RMDIR /S /Q node_modules
+RMDIR /S /Q dist
+RMDIR /S /Q yarn-error.log
+RMDIR /S /Q *.db
 CALL yarn cache clean
---
+++ src/common.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export const DETAIL_DB_NAME = 'detail.db';
+export const PERSONALITY_DB_NAME = 'personality.db';
+export const FIRST_DATE = '1873-02-01';
