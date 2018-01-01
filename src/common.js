1dc8a620d565e5df97a0a617907e0bd5d0bb3efe
Shuhei Nomura <info@danmaq.com>
2018-01-02 00:26:34 +0900

    Changed path of database.

--- src/common.js
+++ src/common.js
@@ -1,5 +1,18 @@
 'use strict';
 
-export const DETAIL_DB_NAME = 'detail.db';
-export const PERSONALITY_DB_NAME = 'personality.db';
+import path from 'path';
+
+/**
+ * Create path.
+ * @param {string} file File name.
+ */
+const createPath = file => path.join(__dirname, file);
+
+/** File name of detail database. */
+export const DETAIL_DB_NAME = createPath('detail.db');
+
+/** File name of personality database. */
+export const PERSONALITY_DB_NAME = createPath('personality.db');
+
+/** First available data. */
 export const FIRST_DATE = '1873-02-01';
